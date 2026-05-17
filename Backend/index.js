b20ff283d464645349527f807ec336e6eac547a9
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-17 22:12:50 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,16 @@
-const express=require('express');
 const dotenv=require('dotenv');
 dotenv.config();
+const express=require('express');
+
+const errorHandler=require('./middleware/errorHandler')
+
 
 const authRoutes=require('./routes/auth');
 
 const app=express();
 app.use(express.json());
+app.unsubscribe(errorHandler);
 app.use('/auth',authRoutes);
 
-// app.unsubscribe(errorHandler);
 const PORT=process.env.PORT|| 3000;
 app.listen(PORT,()=>console.log(`Server running on PORT ${PORT}`))
