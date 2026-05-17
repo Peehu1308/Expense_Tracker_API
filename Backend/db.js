83bdc861cd73ee5336fb3713afa57899f35adeab
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-17 22:12:43 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,4 +1,7 @@
-const {PrismaClient}=require('@prisma/client')
+const { PrismaClient } = require('@prisma/client')
 
-const prisma=new PrismaClient()
-module.exports=prisma
+const prisma = new PrismaClient({
+  datasourceUrl: process.env.DATABASE_URL,
+})
+
+module.exports = prisma
