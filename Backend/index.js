442f3bfbbc9906a639a9f61b5db44a17182483ca
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-18 21:42:42 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -9,8 +9,9 @@ const authRoutes=require('./routes/auth');
 
 const app=express();
 app.use(express.json());
-app.unsubscribe(errorHandler);
+
 app.use('/auth',authRoutes);
 
+app.use(errorHandler);
 const PORT=process.env.PORT|| 3000;
 app.listen(PORT,()=>console.log(`Server running on PORT ${PORT}`))
