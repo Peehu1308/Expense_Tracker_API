183910570a607d963ff5163e8bb9d80e093d3a5a
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-18 23:26:02 +0530

    Create expenses.js

---
+++ Backend/routes/expenses.js
@@ -0,0 +1,41 @@
+const express=require('express')
+const router=express.Router();
+const {body,validationResult}=require('express-validator');
+const Expenses=require('../models/expense.model')
+
+router.post('/add_expense',[
+    body('amount').isNumeric(),
+    body('category').notEmpty(),
+],async(req,res,next)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty())return res.status(400).json({errors:errors.array()});
+
+    try{
+        const {amount,category,note}=req.body;
+        const expenses=await Expenses.create({
+            amount,category,note
+        });
+
+        res.status(200).json({message:"code is working"})
+    }
+    catch(err){
+        next(err);
+
+    }
+});
+
+router.get("/get_expense",async(req,res,next)=>{
+    try{
+        const expenses=await Expenses.find();
+
+        res.status(200).json({
+            message:"Expenses fetched successfully",
+            data:expenses
+        });
+    }
+    catch(err){
+        next(err);
+    }
+})
+
+module.exports=router;
