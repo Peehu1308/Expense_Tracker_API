c5f7250d8fe489a4013e5a145c434be11ae13573
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-18 23:43:27 +0530

    Update expenses.js

--- Backend/routes/expenses.js
+++ Backend/routes/expenses.js
@@ -26,7 +26,7 @@ router.post('/add_expense',[
 
 router.get("/get_expense",async(req,res,next)=>{
     try{
-        const expenses=await Expenses.find();
+        const expenses=await Expenses.find().sort({createdAt:-1});
 
         res.status(200).json({
             message:"Expenses fetched successfully",
@@ -36,6 +36,25 @@ router.get("/get_expense",async(req,res,next)=>{
     catch(err){
         next(err);
     }
+});
+
+router.get("/get_expenses",async(req,res,next)=>{
+    try{
+        const {category}=req.query;
+        let filter={};
+        if(category){
+            filter.category=category;
+        }
+        const expenses=await Expenses.find(filter);
+        res.status(200).json({data:expenses});
+
+
+
+    }
+    catch(err){
+        next(err);
+    }
 })
+
 
 module.exports=router;
