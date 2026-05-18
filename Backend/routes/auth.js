2d23dac14d257ad2d162b96d264aefca518a0904
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-18 22:37:13 +0530

    Update auth.js

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -3,12 +3,12 @@ const router=express.Router();
 const bcrypt=require('bcryptjs')
 const jwt=require('jsonwebtoken')
 const {body, validationResult}=require('express-validator')
-const prisma=require('../db')
+const User=require('../models/user.model')
 
 
 router.post('/register',[
     body('email').isEmail(),
-    body('password').isLength({min:6}),
+    body('password'),
 ],async(req,res,next)=>{
     const errors=validationResult(req);
     if(!errors.isEmpty())return res.status(400).json({errors:errors.array()});
@@ -16,13 +16,12 @@ router.post('/register',[
     try{
         const {username,email,password}=req.body;
 
-        const existing=await prisma.user.findUnique({where:{email}});
+        const existing=await User.findOne({where:{email}});
         if(existing)return res.status(409).json({error:"Email is already registered"});
         
         const hashedPassword=await bcrypt.hash(password,10);
-        const user=await prisma.user.create({
-            data:{email,password:hashedPassword},
-            select:{id:true,email:true},
+        const user=await User.create({
+           email,password:hashedPassword,
         });
 
         res.status(201).json({user});
@@ -41,14 +40,14 @@ router.post('/login',[
 
     try{
         const {username,email,password}=req.body;
-        const user=await prisma.user.findUnique({where:{email}});
+        const user=await User.findOne({where:{email}});
         if(!user)return res.status(404).json({error:"user not found"});
 
         const isMatch=await bcrypt.compare(password,user.password);
         if(!isMatch) return res.status(404).json({error:"invalid credentials"});
 
         const token=jwt.sign(
-            {userId:user.id,email:user.email},
+            {userId:user._id,email:user.email},
             process.env.JWT_SECRET,
             {expiresIn:'7d'}
         );
