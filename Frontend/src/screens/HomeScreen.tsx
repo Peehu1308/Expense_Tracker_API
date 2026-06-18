6f22bb137922cd08f6d349bdfb484ea724df3ba4
Peehu1308 <peehumishra1308@gmail.com>
2026-06-18 23:49:48 +0530

    Frontend Boiler Code

---
+++ Frontend/src/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+
+const HomeScreen = () => {
+  return (
+    <div className="flex justify-between">
+        <Sidebar/>
+    check
+    </div>
+  )
+}
+
+export default HomeScreen
