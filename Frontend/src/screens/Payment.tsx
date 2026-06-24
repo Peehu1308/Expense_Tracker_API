1414e9f5788bec98f4889968f64c32deb62257e3
Peehu1308 <peehumishra1308@gmail.com>
2026-06-24 10:01:10 +0530

    Update-Frontend

--- Frontend/src/screens/Payment.tsx
+++ Frontend/src/screens/Payment.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Payment = () => {
+  const [amount,setamount]=useState("");
+  const [note,setnote]=useState("");
+  const [category,setcategory]=useState("");
+
+  const addExpense=async()=>{
+    const res=await fetch('/expenses/add_expense',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({amount,category,note})
+    });
+    const data=await res.json();
+    console.log(data);
+    setamount("");
+    setcategory("");
+    setnote("");
+
+  }
+
   return (
-    <div>Payment</div>
+    <div>Payment
+
+      <input type='number' placeholder='Input the expense' value={amount} onChange={(e)=>setamount(e.target.value)}/>
+      <input type='text' placeholder='category' value={category} onChange={(e)=>setcategory(e.target.value)}/>
+      <input type='text' placeholder='Note' value={note} onChange={(e)=>setnote(e.target.value)}/>
+      <button onClick={addExpense}>Add expense</button>
+
+    </div>
   )
 }
 
